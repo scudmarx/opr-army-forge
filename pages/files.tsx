35d0c6e55dc40b11bb22f6c5a84107f30ea74534
Adam Lay <adam93.lay+git@gmail.com>
2021-09-15 22:22:57 +0100

    Army File selection page

---
+++ pages/files.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from 'react-redux'
+import { RootState } from '../data/store'
+import { setArmyFile } from '../data/armySlice'
+import { useRouter } from 'next/router';
+import { Button } from "@material-ui/core";
+
+export default function Files() {
+
+    const army = useSelector((state: RootState) => state.army);
+    const [armyFiles, setArmyFiles] = useState(null);
+    const dispatch = useDispatch();
+    const router = useRouter();
+
+    useEffect(() => {
+        // TODO: Test only, add army selection
+        fetch("definitions/army-files.json")
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                setArmyFiles(data);
+            });
+    }, []);
+
+    const selectArmy = (filePath: string) => {
+        dispatch(setArmyFile(filePath));
+        router.push('/list');
+    }
+
+    return (
+        <div className="container">
+            <div className="card mx-auto mt-6" style={{ maxWidth: "480px" }}>
+                <div className="card-content">
+                    <h3 className="is-size-4 has-text-centered mb-4">Select Army List</h3>
+                    <ul>
+                        {
+                            !armyFiles ? null : armyFiles[army.gameSystem].map((file, index) => (
+                                <li key={index} className="mb-4">
+                                    <Button variant="contained" color="primary" onClick={() => selectArmy(file.path)}>
+                                        {file.name}
+                                    </Button>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,38 @@
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { RootState } from '../data/store'
-import GameSystemSelection from "../views/GameSystemSelection";
-import ArmyFileSelection from "../views/ArmyFileSelection";
+import { setGameSystem, setArmyFile } from '../data/armySlice'
+import 'react-spring-bottom-sheet/dist/style.css'
+import { useRouter } from 'next/router';
 
 export default function Home() {
 
   const army = useSelector((state: RootState) => state.army);
+  const dispatch = useDispatch();
+  const router = useRouter();
+
+  const selectGameSystem = (gameSystem: string) => {
+    dispatch(setGameSystem(gameSystem));
+    router.push("/files");
+  };
 
   return (
     <div className="container">
-      {army.gameSystem == null ? <GameSystemSelection /> : <ArmyFileSelection />}
+      <div className="card mx-auto mt-6" style={{ maxWidth: "480px" }}>
+        <div className="card-content">
+          <h3 className="is-size-4 has-text-centered mb-4">Select Game System</h3>
+          <div className="columns is-multiline">
+            {
+              // For each game system
+              ["gf", "gff", "aof", "aofs"].map(gameSystem => (
+                <div key={gameSystem} className="column is-half">
+                  <img onClick={() => selectGameSystem(gameSystem)} src={`img/${gameSystem}_cover.jpg`} className="game-system-tile" />
+                </div>
+              ))
+            }
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
