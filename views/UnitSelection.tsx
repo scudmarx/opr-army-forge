55fcddf8e7eca13fc31435e7d555da15947421bb
Adam Lay <adam93.lay+git@gmail.com>
2021-09-16 09:46:58 +0100

    Army view accordion

--- views/UnitSelection.tsx
+++ views/UnitSelection.tsx
@@ -3,9 +3,10 @@ import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../data/store';
 import { addUnit } from '../data/listSlice';
 import { groupBy } from "../services/Helpers";
-import { Fragment } from "react";
-import { Button, Fab, IconButton } from "@material-ui/core";
+import { Fragment, useState } from "react";
+import { Accordion, AccordionDetails, AccordionSummary, Button, Fab, IconButton } from "@material-ui/core";
 import AddIcon from '@material-ui/icons/Add';
+import EquipmentService from "../services/EquipmentService";
 
 export function UnitSelection({ onSelected }) {
 
@@ -13,6 +14,7 @@ export function UnitSelection({ onSelected }) {
   const armyData = useSelector((state: RootState) => state.army);
   const army = armyData.data;
   const dispatch = useDispatch();
+  const [expandedId, setExpandedId] = useState(null);
 
   // If army has not been selected yet, show nothing
   if (!armyData.loaded)
@@ -35,6 +37,7 @@ export function UnitSelection({ onSelected }) {
       <h3 className="is-size-4">
         {army.name} - v{army.version}
       </h3>
+
       {
         // For each category
         Object.keys(unitGroups).map(key => (
@@ -43,33 +46,40 @@ export function UnitSelection({ onSelected }) {
             <ul className="menu-list">
               {
                 // For each unit in category
-                unitGroups[key].map((u) => (
-                  <li key={u.name} className="mb-2" style={{ cursor: "pointer" }}>
-                    <div className="is-flex is-align-items-center">
-                      <div className="is-flex-grow-1">
-                        <p className="mb-1" style={{ fontWeight: 600 }}>{u.name} {u.size > 1 ? `[${u.size}]` : ''}</p>
-                        <div className="is-flex" style={{ fontSize: "14px", color: "#666" }}>
-                          <p>Qua {u.quality}</p>
-                          <p className="ml-2">Def {u.defense}</p>
+                unitGroups[key].map((u, index) => (
+                  <Accordion key={u.name} square expanded={expandedId === u.name} onChange={() => setExpandedId(u.name)}>
+                    <AccordionSummary>
+                      <div className="is-flex is-flex-grow-1 is-align-items-center">
+                        <div className="is-flex-grow-1" onClick={() => setExpandedId(u.name)}>
+                          <p className="mb-1" style={{ fontWeight: 600 }}>{u.name} {u.size > 1 ? `[${u.size}]` : ''}</p>
+                          <div className="is-flex" style={{ fontSize: "14px", color: "#666" }}>
+                            <p>Qua {u.quality}</p>
+                            <p className="ml-2">Def {u.defense}</p>
+                          </div>
                         </div>
+                        <p className="mr-2">{u.cost}pts</p>
+                        <IconButton color="primary" onClick={(e) => { e.stopPropagation(); handleSelection(u); }}>
+                          <AddIcon />
+                        </IconButton>
                       </div>
-                      <p className="mr-2">{u.cost}pts</p>
-                      <IconButton color="primary" onClick={() => handleSelection(u)}>
-                        <AddIcon />
-                      </IconButton>
-                      {/* <Fab color="default" size="small" onClick={() => handleSelection(u)} >
-                        
-                      </Fab> */}
-                    </div>
-                    <hr className="my-1" />
-                    {/* <a >{u.name} [{u.size}] {u.quality} {u.defense} {u.cost}pt</a> */}
-                  </li>
+                    </AccordionSummary>
+                    <AccordionDetails style={{ flexDirection: "column" }}>
+                      <div className="mb-2">{(u.specialRules || []).join(", ")}</div>
+                      <div>
+                        {u.equipment.filter(eqp => eqp.count > 0).map((eqp, i) => (
+                          <span key={i}>
+                            {(eqp.count && eqp.count !== 1 ? `${eqp.count}x ` : "") + EquipmentService.formatString(eqp)}
+                          </span>
+                        ))}
+                      </div>
+                    </AccordionDetails>
+                  </Accordion>
                 ))
               }
             </ul>
           </Fragment>
         ))
       }
-    </aside>
+    </aside >
   );
 }
