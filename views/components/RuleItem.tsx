3ede04acae90aee553cd09774865562fc6ae1517
Adam Lay <adam93.lay+git@gmail.com>
2021-10-02 12:07:25 +0100

    Rule underline offset

--- views/components/RuleItem.tsx
+++ views/components/RuleItem.tsx
@@ -3,7 +3,7 @@ import { Tooltip } from "@mui/material";
 export default function RuleItem({ label, description }) {
     return (
         <Tooltip title={description} arrow>
-            <span style={{ textDecoration: "underline", textDecorationStyle: "dashed", textDecorationColor: "#666" }}>{label}</span>
+            <span style={{ textDecoration: "underline", textDecorationStyle: "dashed", textDecorationColor: "#666", textUnderlineOffset: "4px" }}>{label}</span>
         </Tooltip>
     );
 }
