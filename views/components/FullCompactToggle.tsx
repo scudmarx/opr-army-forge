f68f6366061879b6b9909436e50b3f5df2815a09
Adam Lay <adam93.lay+git@gmail.com>
2021-10-09 12:01:13 +0100

    #56 Full Compact icons

--- views/components/FullCompactToggle.tsx
+++ views/components/FullCompactToggle.tsx
@@ -1,14 +1,15 @@
 import { Button } from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
+import ViewFullIcon from "../icons/view_full";
+import ViewCompactIcon from "../icons/view_compact";
 
 export default function FullCompactToggle({ expanded, onToggle }: { expanded: boolean, onToggle: () => void }) {
-    return (
-        <div className="is-flex px-4" style={{ alignItems: "center" }}>
-            <p className="is-flex-grow-1" style={{ fontWeight: 600 }}>Units</p>
-            <Button onClick={onToggle}>
-                <MenuIcon />
-                <span className="pl-2 full-compact-text">{expanded ? "Full" : "Compact"}</span>
-            </Button>
-        </div>
-    );
+  return (
+    <div className="is-flex px-4" style={{ alignItems: "center" }}>
+      <p className="is-flex-grow-1" style={{ fontWeight: 600 }}>Units</p>
+      <Button onClick={onToggle}>
+        {expanded ? <ViewFullIcon /> : <ViewCompactIcon />}
+        <span className="pl-1 full-compact-text">{expanded ? "Full" : "Compact"}</span>
+      </Button>
+    </div>
+  );
 }
---
+++ views/icons/view_compact.js
@@ -0,0 +1,16 @@
+export default function ViewCompactIcon() {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M5 13H19V11H5V13ZM5 17H19V15H5V17ZM5 7V9H19V7H5ZM5 13H19V11H5V13ZM5 17H19V15H5V17ZM5 7V9H19V7H5Z"
+        fill="#1976d2"
+      />
+    </svg>
+  );
+}
