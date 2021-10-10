7caa8255cc30ad4e07bd9b6d78aaaee9b34bcc6b
Adam Lay <adam93.lay+git@gmail.com>
2021-10-10 12:33:59 +0100

    #71 Sticky headers

--- views/components/MainMenu.tsx
+++ views/components/MainMenu.tsx
@@ -11,88 +11,88 @@ import UpgradeService from "../../services/UpgradeService";
 
 export default function MainMenu() {
 
-    const list = useSelector((state: RootState) => state.list);
-    const dispatch = useDispatch();
-    const router = useRouter();
-    const [anchorEl, setAnchorEl] = useState(null);
+  const list = useSelector((state: RootState) => state.list);
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const [anchorEl, setAnchorEl] = useState(null);
 
-    const handleMenu = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
 
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-    const handleSave = () => {
-        const unitsJson = JSON.stringify(list.units);
-        const saveName = prompt("Saved list name:");
-        localStorage["AF_Save_" + saveName] = unitsJson;
-    };
+  const handleSave = () => {
+    const unitsJson = JSON.stringify(list.units);
+    const saveName = prompt("Saved list name:");
+    localStorage["AF_Save_" + saveName] = unitsJson;
+  };
 
-    const handleLoad = () => {
-        const loadName = prompt("Load list name:");
-        const units = JSON.parse(localStorage["AF_Save_" + loadName]);
-        dispatch(loadSavedList(units));
-    };
+  const handleLoad = () => {
+    const loadName = prompt("Load list name:");
+    const units = JSON.parse(localStorage["AF_Save_" + loadName]);
+    dispatch(loadSavedList(units));
+  };
 
-    const points = UpgradeService.calculateListTotal(list.units);
+  const points = UpgradeService.calculateListTotal(list.units);
 
-    return (
-        <AppBar position="static" elevation={0}>
-            <Toolbar>
-                <IconButton
-                    size="large"
-                    edge="start"
-                    color="inherit"
-                    aria-label="menu"
-                    sx={{ mr: 2 }}
-                    onClick={() => router.back()}
-                >
-                    <BackIcon />
-                </IconButton>
-                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                    {list.name} • {points}pts
-                </Typography>
-                <IconButton
-                    size="large"
-                    edge="start"
-                    color="inherit"
-                    aria-label="menu"
-                    onClick={() => router.push("/cards")}
-                    className="mr-4"
-                >
-                    <VisibilityIcon />
-                </IconButton>
-                <IconButton
-                    size="large"
-                    edge="start"
-                    color="inherit"
-                    aria-label="menu"
-                    onClick={handleMenu}
-                >
-                    <MoreVertIcon />
-                </IconButton>
-                <Menu
-                    id="menu-appbar"
-                    anchorEl={anchorEl}
-                    anchorOrigin={{
-                        vertical: 'top',
-                        horizontal: 'right',
-                    }}
-                    keepMounted
-                    transformOrigin={{
-                        vertical: 'top',
-                        horizontal: 'right',
-                    }}
-                    open={Boolean(anchorEl)}
-                    onClose={handleClose}
-                >
-                    <MenuItem onClick={() => router.push("/cards")}>View Cards</MenuItem>
-                    <MenuItem onClick={handleSave}>Save List</MenuItem>
-                    <MenuItem onClick={handleLoad}>Load List</MenuItem>
-                </Menu>
-            </Toolbar>
-        </AppBar>
-    );
+  return (
+    <AppBar elevation={0} style={{ position: "sticky", top: 0, zIndex: 1 }}>
+      <Toolbar>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+          onClick={() => router.back()}
+        >
+          <BackIcon />
+        </IconButton>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          {list.name} • {points}pts
+        </Typography>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          onClick={() => router.push("/cards")}
+          className="mr-4"
+        >
+          <VisibilityIcon />
+        </IconButton>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          onClick={handleMenu}
+        >
+          <MoreVertIcon />
+        </IconButton>
+        <Menu
+          id="menu-appbar"
+          anchorEl={anchorEl}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem onClick={() => router.push("/cards")}>View Cards</MenuItem>
+          <MenuItem onClick={handleSave}>Save List</MenuItem>
+          <MenuItem onClick={handleLoad}>Load List</MenuItem>
+        </Menu>
+      </Toolbar>
+    </AppBar>
+  );
 }
--- views/listBuilder/MobileView.tsx
+++ views/listBuilder/MobileView.tsx
@@ -18,93 +18,93 @@ import MainMenu from "../components/MainMenu";
 
 export default function MobileView() {
 
-    const list = useSelector((state: RootState) => state.list);
-    const army = useSelector((state: RootState) => state.army);
+  const list = useSelector((state: RootState) => state.list);
+  const army = useSelector((state: RootState) => state.army);
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const [slider, setSlider] = useState(null);
-    const [sheetOpen, setSheetOpen] = useState(false);
-    const [slideIndex, setSlideIndex] = useState(1);
+  const [slider, setSlider] = useState(null);
+  const [sheetOpen, setSheetOpen] = useState(false);
+  const [slideIndex, setSlideIndex] = useState(1);
 
 
-    // Open bottom sheet when unit is selected
-    const onUnitSelected = () => {
-        setSheetOpen(true);
-    };
+  // Open bottom sheet when unit is selected
+  const onUnitSelected = () => {
+    setSheetOpen(true);
+  };
 
-    // Reset selected unit when sheet is closed
-    function onDismissSheet() {
-        setSheetOpen(false);
-        dispatch(selectUnit(null));
-    }
+  // Reset selected unit when sheet is closed
+  function onDismissSheet() {
+    setSheetOpen(false);
+    dispatch(selectUnit(null));
+  }
 
-    // Keep Tab bar and carousel in sync
-    const handleSlideChange = (event, newValue) => {
-        setSlideIndex(newValue);
-        slider.slickGoTo(newValue);
-    };
+  // Keep Tab bar and carousel in sync
+  const handleSlideChange = (event, newValue) => {
+    setSlideIndex(newValue);
+    slider.slickGoTo(newValue);
+  };
 
-    const selectedUnit = list.selectedUnitId === null || list.selectedUnitId === undefined
-        ? null
-        : list.units.filter(u => u.selectionId === list.selectedUnitId)[0];
+  const selectedUnit = list.selectedUnitId === null || list.selectedUnitId === undefined
+    ? null
+    : list.units.filter(u => u.selectionId === list.selectedUnitId)[0];
 
-    // Slick carousel settings
-    const sliderSettings = {
-        dots: false,
-        slidesToShow: 1,
-        infinite: false,
-        arrows: false,
-        initialSlide: 1,
-        beforeChange: (current, next) => handleSlideChange(null, next)
-    };
+  // Slick carousel settings
+  const sliderSettings = {
+    dots: false,
+    slidesToShow: 1,
+    infinite: false,
+    arrows: false,
+    initialSlide: 1,
+    beforeChange: (current, next) => handleSlideChange(null, next)
+  };
 
-    return (
-        <>
-            <Paper elevation={1} color="primary" square>
-                <MainMenu />
-                <AppBar elevation={0} position="static" style={{ position: "sticky", top: 0, zIndex: 1 }}>
-                    <Tabs value={slideIndex} onChange={handleSlideChange} centered variant="fullWidth" textColor="inherit" indicatorColor="primary">
-                        <Tab label={`${army?.data?.name} v${army?.data?.version}`} />
-                        <Tab label={`${list.name} - ${UpgradeService.calculateListTotal(list.units)}pts`} />
-                    </Tabs>
-                </AppBar>
-            </Paper>
+  return (
+    <>
+      <Paper elevation={1} color="primary" square style={{ position: "sticky", top: 0, zIndex: 1 }}>
+        <MainMenu />
+        <AppBar elevation={0} style={{ position: "sticky", top: 0, zIndex: 1 }}>
+          <Tabs value={slideIndex} onChange={handleSlideChange} centered variant="fullWidth" textColor="inherit" indicatorColor="primary">
+            <Tab label={`${army?.data?.name} v${army?.data?.version}`} />
+            <Tab label={`${list.name} - ${UpgradeService.calculateListTotal(list.units)}pts`} />
+          </Tabs>
+        </AppBar>
+      </Paper>
 
-            <Slider {...sliderSettings} ref={slider => setSlider(slider)} style={{ maxHeight: "100%" }}>
-                <div>
-                    <UnitSelection onSelected={() => { }} />
-                </div>
-                <div className="">
-                    {list.units.length > 0 ? <MainList onSelected={onUnitSelected} /> : (
-                        <div className="p-4 has-text-centered">
-                            <h3 className="is-size-3 mb-4">Your list is empty</h3>
-                            <Button variant="outlined" onClick={() => handleSlideChange(null, 0)}>
-                                <Add /> Add Units
-                            </Button>
-                        </div>
-                    )}
-                </div>
-            </Slider>
+      <Slider {...sliderSettings} ref={slider => setSlider(slider)} style={{ maxHeight: "100%" }}>
+        <div>
+          <UnitSelection onSelected={() => { }} />
+        </div>
+        <div className="">
+          {list.units.length > 0 ? <MainList onSelected={onUnitSelected} /> : (
+            <div className="p-4 has-text-centered">
+              <h3 className="is-size-3 mb-4">Your list is empty</h3>
+              <Button variant="outlined" onClick={() => handleSlideChange(null, 0)}>
+                <Add /> Add Units
+              </Button>
+            </div>
+          )}
+        </div>
+      </Slider>
 
-            <BottomSheet
-                open={sheetOpen}
-                onDismiss={onDismissSheet}
-                initialFocusRef={false}
-                expandOnContentDrag={true}
-                onScrollCapture={(e) => e.preventDefault()}
-                defaultSnap={({ snapPoints, lastSnap }) =>
-                    lastSnap ?? Math.min(...snapPoints)
-                }
-                snapPoints={({ minHeight, maxHeight }) => [
-                    minHeight,
-                    maxHeight * 0.9
-                ]}
-                header={
-                    <UpgradePanelHeader selectedUnit={selectedUnit} />
-                }>
-                <Upgrades />
-            </BottomSheet>
-        </>
-    );
+      <BottomSheet
+        open={sheetOpen}
+        onDismiss={onDismissSheet}
+        initialFocusRef={false}
+        expandOnContentDrag={true}
+        onScrollCapture={(e) => e.preventDefault()}
+        defaultSnap={({ snapPoints, lastSnap }) =>
+          lastSnap ?? Math.min(...snapPoints)
+        }
+        snapPoints={({ minHeight, maxHeight }) => [
+          minHeight,
+          maxHeight * 0.9
+        ]}
+        header={
+          <UpgradePanelHeader selectedUnit={selectedUnit} />
+        }>
+        <Upgrades />
+      </BottomSheet>
+    </>
+  );
 }
