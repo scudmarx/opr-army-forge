54b89036613cad1c3dee974f48412d28e072c606
Adam Lay <adam93.lay+git@gmail.com>
2021-10-20 22:34:53 +0100

    #73 Fix

--- views/components/RuleItem.tsx
+++ views/components/RuleItem.tsx
@@ -6,7 +6,7 @@ export default function RuleItem({ label, description }) {
   const descParts = description.split(bullet).map(part => (<p>{part}</p>));
 
   return (
-    <CustomTooltip title={descParts} arrow enterTouchDelay={0} leaveTouchDelay={10000}>
+    <CustomTooltip title={descParts} arrow enterTouchDelay={0} leaveTouchDelay={10000} onClick={e => e.stopPropagation()}>
       <span style={{ textDecoration: "underline", textDecorationStyle: "dashed", textDecorationColor: "#666", textUnderlineOffset: "4px" }}>{label}</span>
     </CustomTooltip>
   );
