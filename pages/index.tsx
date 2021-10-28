e1d68aba74adfb4123207525b4d81a0c8e13a500
Adam Lay <adam93.lay+git@gmail.com>
2021-10-28 23:51:00 +0100

    Beta

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router';
 import { Button } from '@mui/material';
 import FolderOpenIcon from '@mui/icons-material/FolderOpen';
 import AddIcon from '@mui/icons-material/Add';
+import { style } from "@mui/system";
 
 export default function Home() {
 
@@ -10,15 +11,21 @@ export default function Home() {
 
   return (
     <>
-      <div className="container">
-        <div className="mx-auto p-4" style={{ maxWidth: "480px" }}>
-          <h1 className="is-size-3 has-text-centered mb-4">Army Forge</h1>
-          <div className={styles.homeContainer + " is-flex is-flex-direction-column p-4"}>
+      <div className={styles.homeContainer + " container"}>
+        <div className="mx-auto has-text-centered p-4" style={{ maxWidth: "480px", height: "100%", position: "relative" }}>
+          <h1 className={styles.title} style={{ letterSpacing: "8px" }}>ARMY</h1>
+          <div className={styles.logo + " mx-auto"}></div>
+          <h1 className={styles.title}>
+            FORGE
+            <div className={styles.betaTag}></div>
+          </h1>
+
+          <div className={styles.buttonContainer + " is-flex is-flex-direction-column p-4"}>
             <Button variant="contained" color="primary" className="mb-4" onClick={() => router.push("/gameSystem")}>
-              <AddIcon /> <span className="ml-2" style={{fontWeight:600}}>Create A New List</span>
+              <AddIcon /> <span className="ml-2" style={{ fontWeight: 600 }}>Create A New List</span>
             </Button>
-            <Button variant="outlined" onClick={() => router.push("/load")}>
-              <FolderOpenIcon /> <span className="ml-2" style={{fontWeight:600}}>Open A List</span>
+            <Button variant="outlined" color="info" onClick={() => router.push("/load")}>
+              <FolderOpenIcon /> <span className="ml-2" style={{ fontWeight: 600 }}>Open A List</span>
             </Button>
           </div>
         </div>
