ee354673d66b4dc79df11b4ae80de9075b65a9ef
scudmarx <darth_dunk@hotmail.com>
2021-11-09 20:24:15 +0000

    Got box-shadows to only show when scrolled.

--- views/MainList.tsx
+++ views/MainList.tsx
@@ -29,7 +29,7 @@ export function MainList({ onSelected, onUnitRemoved, mobile=false }) {
 
   return (
     <>
-      <div className="sticky">
+      <div id="MainListHeader" className="sticky">
         <h3 className="px-4 pt-4 is-size-4 is-hidden-mobile">{`My List - ${list.points}` + (list.pointsLimit ? `/${list.pointsLimit}` : "") + "pts"}</h3>
         <FullCompactToggle expanded={expandAll} onToggle={() => setExpandAll(!expandAll)} />
       </div>
--- views/listBuilder/DesktopView.tsx
+++ views/listBuilder/DesktopView.tsx
@@ -20,16 +20,20 @@ export default function DesktopView() {
 
   const columnStyle: any = { overflowY: "scroll", maxHeight: "100%" };
 
+  const setShadow = (header, scroller) => () => {
+    (document.getElementById(header).style.boxShadow = document.getElementById(scroller).scrollTop ? "0px 3px 5px 0px lightgrey" : "")
+  }
+
   return (
     <>
       <Paper elevation={1} color="primary" square>
         <MainMenu setListConfigurationOpen={setEditListOpen} setValidationOpen={setValidationOpen} />
       </Paper>
       <div className="columns my-0" style={{ height: "calc(100vh - 64px)" }}>
-        <div className="column py-0 pr-0" style={columnStyle}>
+        <div id="ColumnOne" className="column py-0 pr-0" style={columnStyle} onScroll={setShadow("UnitSelectionHeader", "ColumnOne")}>
           <UnitSelection onSelected={() => { }} />
         </div>
-        <div className="column p-0" style={columnStyle}>
+        <div id="ColumnTwo" className="column p-0" style={columnStyle} onScroll={setShadow("MainListHeader", "ColumnTwo")}>
           <MainList onSelected={() => { }} onUnitRemoved={() => setShowUndoRemove(true)} />
         </div>
         <div className="column py-0 px-0 mr-4" style={columnStyle}>
