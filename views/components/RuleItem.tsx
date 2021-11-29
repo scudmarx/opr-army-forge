978e1750ac8253a47db94fc8f1a002f0bf166f44
scudmarx <darth_dunk@hotmail.com>
2021-11-29 17:28:57 +0000

    fix.
    *sigh*

--- views/components/RuleItem.tsx
+++ views/components/RuleItem.tsx
@@ -5,11 +5,11 @@ export default function RuleItem({ label, description }) {
   const bullet = /•|/;
   const descParts = description.split(bullet).map(part => (<p>{part}</p>));
 
-  return (
-    description ?
+  let content = description ?
     <CustomTooltip title={descParts} arrow enterTouchDelay={0} leaveTouchDelay={10000} onClick={e => e.stopPropagation()}>
       <span style={{ textDecoration: "underline", textDecorationStyle: "dashed", textDecorationColor: "#666", textUnderlineOffset: "4px" }}>{label}</span>
     </CustomTooltip>
-    : label
-  );
+    : <span>{label}</span>
+
+  return content;
 }
