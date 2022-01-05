4dbaaeef77d9c8baf0c70aadcf4a978989f6c351
Adam Lay <adam93.lay+git@gmail.com>
2022-01-05 13:46:26 +0000

    Update UnitSelection.tsx

--- views/UnitSelection.tsx
+++ views/UnitSelection.tsx
@@ -53,9 +53,9 @@ export function UnitSelection({ onSelected, addUnit = (unit: IUnit, dummy = fals
       unitGroups["Aircraft"].push(unit);
     else if (hasRule(unit, "Artillery"))
       unitGroups["Artillery"].push(unit);
-    else if (isTough(unit, 18) && unit.defense == "2")
+    else if (isTough(unit, 18) && unit.defense == "2" && unit.size === 1)
       unitGroups["Titans"].push(unit);
-    else if (isTough(unit, 6) && unit.defense == "2")
+    else if (isTough(unit, 6) && unit.defense == "2" && unit.size === 1)
       unitGroups["Vehicles / Monsters"].push(unit);
     else
       unitGroups["Core Units"].push(unit);
