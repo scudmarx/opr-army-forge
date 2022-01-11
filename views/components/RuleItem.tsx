bd69316d40e9f214003e62a4df8d4f2f344620bc
scudmarx <darth_dunk@hotmail.com>
2022-01-11 07:42:58 +0000

    Update RuleItem.tsx
    Opening tooltip requires more intentionality. Closing it requires less patience.

--- views/components/RuleItem.tsx
+++ views/components/RuleItem.tsx
@@ -6,7 +6,7 @@ export default function RuleItem({ label, description }) {
   const descParts = description.split(bullet).map(part => (<p>{part}</p>));
 
   let content = description ?
-    <CustomTooltip title={descParts} arrow enterTouchDelay={0} leaveTouchDelay={10000} onClick={e => e.stopPropagation()}>
+    <CustomTooltip title={descParts} arrow enterTouchDelay={1500} leaveTouchDelay={3000} onClick={e => {e.stopPropagation()}}>
       <span style={{ textDecoration: "underline", textDecorationStyle: "dashed", textDecorationColor: "#666", textUnderlineOffset: "4px" }}>{label}</span>
     </CustomTooltip>
     : <span>{label}</span>
